62f5e00d6298e4ecaf641c9e687506d10c5b04f1
jmenacho3 <jesus121078@gmail.com>
2016-12-11 23:18:37 -0500

    Añadiendo los archivos

---
+++ WindowsApplication1/Form1.Designer.vb
@@ -0,0 +1,117 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.txtpeso = New System.Windows.Forms.TextBox()
+        Me.txtalt = New System.Windows.Forms.TextBox()
+        Me.btncalc = New System.Windows.Forms.Button()
+        Me.lblmsg = New System.Windows.Forms.Label()
+        Me.lblres = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(45, 58)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(91, 13)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Ingrese Peso (Kg)"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(45, 116)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(112, 13)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Ingrese Altura (metros)"
+        '
+        'txtpeso
+        '
+        Me.txtpeso.Location = New System.Drawing.Point(199, 58)
+        Me.txtpeso.Name = "txtpeso"
+        Me.txtpeso.Size = New System.Drawing.Size(100, 20)
+        Me.txtpeso.TabIndex = 2
+        '
+        'txtalt
+        '
+        Me.txtalt.Location = New System.Drawing.Point(199, 109)
+        Me.txtalt.Name = "txtalt"
+        Me.txtalt.Size = New System.Drawing.Size(100, 20)
+        Me.txtalt.TabIndex = 3
+        '
+        'btncalc
+        '
+        Me.btncalc.Location = New System.Drawing.Point(48, 215)
+        Me.btncalc.Name = "btncalc"
+        Me.btncalc.Size = New System.Drawing.Size(75, 23)
+        Me.btncalc.TabIndex = 4
+        Me.btncalc.Text = "Calcular"
+        Me.btncalc.UseVisualStyleBackColor = True
+        '
+        'lblmsg
+        '
+        Me.lblmsg.AutoSize = True
+        Me.lblmsg.Location = New System.Drawing.Point(196, 215)
+        Me.lblmsg.Name = "lblmsg"
+        Me.lblmsg.Size = New System.Drawing.Size(0, 13)
+        Me.lblmsg.TabIndex = 5
+        '
+        'lblres
+        '
+        Me.lblres.AutoSize = True
+        Me.lblres.Location = New System.Drawing.Point(196, 174)
+        Me.lblres.Name = "lblres"
+        Me.lblres.Size = New System.Drawing.Size(0, 13)
+        Me.lblres.TabIndex = 6
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(472, 354)
+        Me.Controls.Add(Me.lblres)
+        Me.Controls.Add(Me.lblmsg)
+        Me.Controls.Add(Me.btncalc)
+        Me.Controls.Add(Me.txtalt)
+        Me.Controls.Add(Me.txtpeso)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "Indice Masa Corporal"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents txtpeso As System.Windows.Forms.TextBox
+    Friend WithEvents txtalt As System.Windows.Forms.TextBox
+    Friend WithEvents btncalc As System.Windows.Forms.Button
+    Friend WithEvents lblmsg As System.Windows.Forms.Label
+    Friend WithEvents lblres As System.Windows.Forms.Label
+
+End Class
---
+++ WindowsApplication1/Form1.vb
@@ -0,0 +1,22 @@
+﻿Public Class Form1
+
+    Private Sub btncalc_Click(sender As Object, e As EventArgs) Handles btncalc.Click
+        Dim IMC As Double
+        Dim pesoKG As Double = txtpeso.Text
+        Dim alturaMt As Double = txtalt.Text
+        IMC = pesoKG / (alturaMt * alturaMt)
+
+        If IMC >= 18.5 And IMC <= 24.9 Then
+            lblmsg.Text = "Promedio"
+        ElseIf IMC >= 25 And IMC <= 29.9 Then
+            lblmsg.Text = "Aumentado"
+        ElseIf IMC >= 30 And IMC <= 34.9 Then
+            lblmsg.Text = "Moderado"
+        ElseIf IMC >= 35 And IMC <= 39.9 Then
+            lblmsg.Text = "Severo"
+        ElseIf IMC >= 40 Then
+            lblmsg.Text = "Muy Severo"
+        End If
+        lblres.Text = IMC
+    End Sub
+End Class
